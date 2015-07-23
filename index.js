e81106e7447312615e29426d3b8e281ee52f61b0
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-07-22 22:32:07 -0700

    Add wiki documentation and code commenting

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+/**
+ * @module satoshi-bitcoin
+ */
 module.exports = {
 
+  /**
+   * Convert Satoshi to Bitcoin
+   * @param {number} satoshi Amount of Satoshi to convert
+   * @returns {number}
+   */
   toBitcoin: function(satoshi) {
     return satoshi / 100000000;
   },
 
+  /**
+   * Convert Bitcoin to Satoshi
+   * @param {number} bitcoin Amount of Bitcoin to convert
+   * @returns {number}
+   */
   toSatoshi: function(bitcoin) {
     return bitcoin * 100000000;
   }
---
+++ wiki/index.md
@@ -0,0 +1,42 @@
+# satoshi-bitcoin
+
+
+
+
+
+* * *
+
+### satoshi-bitcoin.toBitcoin(satoshi) 
+
+Convert Satoshi to Bitcoin
+
+**Parameters**
+
+**satoshi**: `number`, Amount of Satoshi to convert
+
+**Returns**: `number`
+
+
+### satoshi-bitcoin.toSatoshi(bitcoin) 
+
+Convert Bitcoin to Satoshi
+
+**Parameters**
+
+**bitcoin**: `number`, Amount of Bitcoin to convert
+
+**Returns**: `number`
+
+
+
+* * *
+
+
+
+
+
+
+
+
+
+

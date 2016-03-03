d45d174eeaced9cddfc6d58cbdb91fd2fcaf9a2c
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2016-03-03 01:22:49 -0700

    clean up tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,51 +1,49 @@
 var should = require('chai').should();
-var converter = require('../index');
-var toBitcoin = converter.toBitcoin;
-var toSatoshi = converter.toSatoshi;
+var sb = require('../index.js');
 
 describe('#toBitcoin', function() {
   it('converts simple integer amounts', function() {
-    toBitcoin(100000000).should.equal(1);
-    toBitcoin(123456789012345).should.equal(1234567.89012345);
+    sb.toBitcoin(100000000).should.equal(1);
+    sb.toBitcoin(123456789012345).should.equal(1234567.89012345);
   });
   it('converts simple string amounts', function() {
-    toBitcoin('100000000').should.equal(1);
-    toBitcoin('123456789012345').should.equal(1234567.89012345);
+    sb.toBitcoin('100000000').should.equal(1);
+    sb.toBitcoin('123456789012345').should.equal(1234567.89012345);
   });
 
   it('converts to Bitcoin, not to Satoshi', function() {
-    toBitcoin(98765).should.not.equal(9876500000000);
+    sb.toBitcoin(98765).should.not.equal(9876500000000);
   });
 
   it('converts and handles corner case rounding', function() {
-    toBitcoin(46).should.equal(.00000046);
+    sb.toBitcoin(46).should.equal(.00000046);
   });
 
   it('handles TypeError input', function() {
-    toBitcoin.bind(this, true).should.throw('toBitcoin must be called on a number or string');
-    toBitcoin.bind(this, 1.1).should.throw('toBitcoin must be called on a whole number or string format whole number');
+    sb.toBitcoin.bind(this, true).should.throw('toBitcoin must be called on a number or string');
+    sb.toBitcoin.bind(this, 1.1).should.throw('toBitcoin must be called on a whole number or string format whole number');
   });
 });
 
 describe('#toSatoshi', function() {
   it('converts simple integer amounts', function() {
-    toSatoshi(0.00000001).should.equal(1);
-    toSatoshi(98765).should.equal(9876500000000);
+    sb.toSatoshi(0.00000001).should.equal(1);
+    sb.toSatoshi(98765).should.equal(9876500000000);
   });
   it('converts simple string amounts', function() {
-    toSatoshi('0.00000001').should.equal(1);
-    toSatoshi('98765').should.equal(9876500000000);
+    sb.toSatoshi('0.00000001').should.equal(1);
+    sb.toSatoshi('98765').should.equal(9876500000000);
   });
 
   it('converts to Satoshi, not to Bitcoin', function() {
-    toSatoshi(123456789012345).should.not.equal(1234567.89012345);
+    sb.toSatoshi(123456789012345).should.not.equal(1234567.89012345);
   });
 
   it('converts and handles corner case rounding', function() {
-    toSatoshi(4.6).should.equal(460000000);
+    sb.toSatoshi(4.6).should.equal(460000000);
   });
 
   it('handles TypeError input', function() {
-    toSatoshi.bind(this, true).should.throw('toSatoshi must be called on a number or string');
+    sb.toSatoshi.bind(this, true).should.throw('toSatoshi must be called on a number or string');
   });
 });

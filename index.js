d799c386123eefc14a0a690292d2a59b86835a71
Dawson Botsford <DawsonBotsford@gmail.com>
2019-04-21 22:20:21 +1000

    init powerful refactor

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-/**
- * @module satoshi-bitcoin
- */
-
 var Big = require('big.js');
 
-// @private
-var conversion = 100000000;
+// quantity of satoshi in X
+var conversion = {
+  bit: 100000,
+  bitcoin: 100000000,
+  millibitcoin: 1000,
+  centibitcoin: 100,
+  decibitcoin: 10,
+  decabitcoin: 10,
+  kilobitcoin: 1000
+};
 
-// es6 polyfill
-if (!Number.isInteger) {
-  Number.isInteger = function(num) {
-    return typeof num === 'number' && num % 1 === 0;
-  }
-}
+// es6 polyfill if (!Number.isInteger) {
+Number.isInteger = function(num) {
+  return typeof num === 'number' && num % 1 === 0;
+};
 
-// @private
 function toNumber(notNum) {
   return Number(notNum);
 }
 
 module.exports = {
-
   /**
    * Convert Satoshi to Bitcoin
    * @param {number|string} satoshi Amount of Satoshi to convert. Must be a whole number
@@ -35,15 +35,19 @@ module.exports = {
       satoshi = toNumber(satoshi);
       satoshiType = 'number';
     }
-    if (satoshiType !== 'number'){
-      throw new TypeError('toBitcoin must be called on a number or string, got ' + satoshiType);
+    if (satoshiType !== 'number') {
+      throw new TypeError(
+        'toBitcoin must be called on a number or string, got ' + satoshiType
+      );
     }
     if (!Number.isInteger(satoshi)) {
-      throw new TypeError('toBitcoin must be called on a whole number or string format whole number');
+      throw new TypeError(
+        'toBitcoin must be called on a whole number or string format whole number'
+      );
     }
 
     var bigSatoshi = new Big(satoshi);
-    return Number(bigSatoshi.div(conversion));
+    return Number(bigSatoshi.div(conversion.bitcoin));
   },
 
   /**
@@ -59,12 +63,29 @@ module.exports = {
       bitcoin = toNumber(bitcoin);
       bitcoinType = 'number';
     }
-    if (bitcoinType !== 'number'){
-      throw new TypeError('toSatoshi must be called on a number or string, got ' + bitcoinType);
+    if (bitcoinType !== 'number') {
+      throw new TypeError(
+        'toSatoshi must be called on a number or string, got ' + bitcoinType
+      );
     }
 
     var bigBitcoin = new Big(bitcoin);
-    return Number(bigBitcoin.times(conversion));
-  }
+    return Number(bigBitcoin.times(conversion.bitcoin));
+  },
 
+  fromSatoshi: function(satoshi, to) {
+    var satoshiType = typeof satoshi;
+    if (satoshiType === 'string') {
+      satoshi = toNumber(satoshi);
+      satoshiType = 'number';
+    }
+    if (satoshiType !== 'number') {
+      throw new TypeError(
+        'fromSatoshi must be called on a number or string, got ' + satoshiType
+      );
+    }
+    var bigSatoshi = new Big(satoshi);
+
+    return Number(bigSatoshi.div(conversion[to]));
+  },
 };
